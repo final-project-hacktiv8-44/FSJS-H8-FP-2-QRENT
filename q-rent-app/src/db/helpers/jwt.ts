e506d4d55ea9e14d0c7fde6c2e2338c0e9f2dfda
Arma Yoga Pratama <mgarmayogapratama@gmail.com>
2024-02-12 18:22:58 +0700

    Release 1 Api Route Register & Login: done

---
+++ q-rent-app/src/db/helpers/jwt.ts
@@ -0,0 +1,21 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import * as jose from "jose";
+
+// const secret_key = process.env.SECRET_KEY as string;
+const secret_key = "Jurus Ninja" as string;
+
+export function SignToken(payload: JwtPayload) {
+  const token = jwt.sign(payload, secret_key);
+  return token;
+}
+
+export function verifyToken(token: string) {
+  const payload = jwt.verify(token, secret_key);
+  return payload;
+}
+
+export async function verifyJose<T>(token: string) {
+  const secret = new TextEncoder().encode(secret_key);
+  const josePayload = await jose.jwtVerify<T>(token, secret);
+  return josePayload.payload;
+}
---
+++ q-rent-app/src/middleware.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { verifyJose } from "../src/db/helpers/jwt";
+
+export async function middleware(request: NextRequest) {
+  const access_token = cookies().get("Authorization")?.value.split(" ")[1];
+
+  if (!access_token) {
+    return NextResponse.json(
+      {
+        message: "You do not have permission. please login by your account.",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const decoded = await verifyJose<{
+    _id: string;
+    email: string;
+    role: string;
+  }>(access_token);
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-UserId", decoded._id);
+  requestHeaders.set("x-email", decoded.email);
+  requestHeaders.set("x-role", decoded.role);
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+
+  return response;
+}
+
+export const config = {
+  matcher: "/api/booking/:path*",
+};
