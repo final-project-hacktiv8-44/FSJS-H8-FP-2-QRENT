2ee0f761ec21df8824c753671ee73dd78d2e1538
Arma Yoga Pratama <mgarmayogapratama@gmail.com>
2024-02-19 00:14:40 +0700

    Release 17 styling cars detail: done

--- q-rent-app/src/app/travel/[name]/page.tsx
+++ q-rent-app/src/app/travel/[name]/page.tsx
@@ -1,4 +1,8 @@
+"use client";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import { TravelType } from "@/types/type";
+import Loader from "@/components/Home/Loader";
 
 type MyResponse = {
   travel: TravelType;
@@ -11,14 +15,75 @@ async function travelDetail(name: string): Promise<MyResponse> {
 
   return response.json();
 }
-export default async function TravelPage({
-  params,
-}: {
-  params: { name: string };
-}) {
-  const travel = await travelDetail(params.name);
 
-  console.log(travel, "?????");
+export default function TravelPage({ params }: { params: { name: string } }) {
+  const [travel, setTravel] = useState<TravelType | null>(null);
 
-  return <div>Travel Page</div>;
+  useEffect(() => {
+    const fetchTravel = async () => {
+      try {
+        const result = await travelDetail(params.name);
+        setTravel(result.travel);
+      } catch (error) {
+        console.error("Error fetching travel details:", error);
+      }
+    };
+
+    fetchTravel();
+  }, [params.name]);
+
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center flex-col mt-20 bg-white">
+      <div className="bg-white w-full min-h-screen flex justify-center items-center flex-col">
+        <h1 className="text-3xl font-bold text-blue-400 mt-10 mb-10 text-center">
+          Travel Details
+        </h1>
+        {travel && (
+          <motion.div
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "easeIn" }}
+            className="flex flex-col items-center">
+            <motion.img
+              src={travel.image}
+              alt={travel.name}
+              className="w-100 h-auto rounded shadow-lg"
+              whileHover={{ scale: 1.05 }}
+              transition={{ duration: 0.2 }}
+              style={{ objectFit: "cover" }}
+            />
+            <div className="mt-8 text-center">
+              <h1 className="text-3xl font-bold text-blue-400 mb-4">
+                {travel.name}
+              </h1>
+              <h2 className="text-xl mb-4 font-bold text-blue-400">
+                Location: {travel.location}
+              </h2>
+              <div className="flex flex-col items-center justify-center font-bold border border-blue-400">
+                <motion.div
+                  className="bg-gray-100 rounded-lg p-4"
+                  transition={{ duration: 0.2, ease: "easeIn" }}>
+                  <p className="text-lg text-blue-400 mb-4">
+                    {travel.description}
+                  </p>
+                </motion.div>
+              </div>
+            </div>
+          </motion.div>
+        )}
+        {!travel && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="text-center">
+            <p className="text-lg font-bold text-gray-600">
+              Loading...
+              <Loader />
+            </p>
+          </motion.div>
+        )}
+      </div>
+    </div>
+  );
 }
--- q-rent-app/src/components/Cars/detailProduct.tsx
+++ q-rent-app/src/components/Cars/detailProduct.tsx
@@ -29,107 +29,107 @@ export default function DetailCar({ data }: { data: MyResponse }) {
 
   return (
     <div className="flex flex-col mt-[5rem]">
-    <div className="flex flex-row mx-[5rem] mt-[3rem] mb-[5rem]">
-      {/* Tampilan gambar mobil */}
-      <div className="flex flex-col gap-4 mr-[1rem]">
-        {data.car.carImage.map((el, i) => (
-          <div key={i} onClick={() => handleSlideImage(el)}>
-            <img
-              className="w-[14.9rem] h-[10rem] border-2 border-blue-400 cursor-pointer rounded-lg shadow-md transition duration-300 transform hover:scale-110"
-              src={el}
-              alt=""
-            />
+      <div className="flex flex-col">
+        <div className="flex flex-row mx-[5rem] mt-[3rem] mb-[5rem]">
+          <div className="flex flex-col gap-4 mr-[1rem]">
+            {data.car.carImage.map((el, i) => (
+              <div key={i} onClick={() => handleSlideImage(el)}>
+                <img
+                  className="w-[14.9rem] h-[10rem] border-2 border-blue-400 cursor-pointer rounded-lg shadow-md transition duration-300 transform hover:scale-110"
+                  src={el}
+                  alt=""
+                />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-  
-      {/* Tampilan gambar besar mobil */}
-      <div className="flex flex-col gap-4 mr-[3rem]">
-        <div className="border border-blue-400 rounded-lg shadow-md p-4">
-          <img
-            className="w-[47rem] h-[30rem] rounded-lg shadow-md transition duration-300 transform"
-            src={imageSlide}
-            alt=""
-          />
-        </div>
-      </div>
-  
-      {/* Penjelasan dan tombol booking */}
-      <div className="border border-blue-400 rounded-lg p-8">
-        <div className="flex flex-col gap-5 text-black">
-          <div className="flex flex-col gap-2">
-            <h3 className="font-secondary text-[2rem]">{data.car.brand}</h3>
-            <h3 className="font-secondary text-[2rem]">{data.car.name}</h3>
-            <h4 className="text-[1rem]">{prices}</h4>
-            <div className="flex flex-row gap-2 items-center">
-              <h4 className="font-semibold">
-                <span className="mr-2">
-                  <IoMdColorPalette className="text-gray-600 inline" />
-                </span>
-                Warna: {data.car.color}
-              </h4>
+
+          <div className="flex flex-col gap-4 mr-[3rem]">
+            <div className="border border-blue-400 rounded-lg shadow-md p-4">
+              <img
+                className="w-[47rem] h-[30rem] rounded-lg shadow-md transition duration-300 transform"
+                src={imageSlide}
+                alt=""
+              />
             </div>
-            <h4 className="font-semibold">
-              <span className="mr-2">
-                <MdPlace className="text-gray-600 inline" />
-              </span>
-              Region: {data.car.region}
-            </h4>
+          </div>
+
+          <div className="border border-blue-400 rounded-lg p-8">
+            <div className="flex flex-col gap-5 text-black">
+              <div className="flex flex-col gap-2">
+                <h3 className="font-secondary text-[2rem]">{data.car.brand}</h3>
+                <h3 className="font-secondary text-[2rem]">{data.car.name}</h3>
+                <h4 className="text-[1rem]">{prices}</h4>
+                <div className="flex flex-row gap-2 items-center">
+                  <h4 className="font-semibold">
+                    <span className="mr-2">
+                      <IoMdColorPalette className="text-gray-600 inline" />
+                    </span>
+                    Warna: {data.car.color}
+                  </h4>
+                </div>
+                <h4 className="font-semibold">
+                  <span className="mr-2">
+                    <MdPlace className="text-gray-600 inline" />
+                  </span>
+                  Region: {data.car.region}
+                </h4>
+                <Link href={`/booking/${data.car._id}`}>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
+                    Booking
+                  </button>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
-  
-        {/* Informasi detail mobil */}
-        <div className="flex flex-col gap-2 border border-blue-400 rounded-lg p-8 text-black">
-          <p className="font-semibold">
-            <span className="mr-2">
-              <IoMdCar className="text-gray-600 inline" />
-            </span>
-            Tipe: {data.car.type}
-          </p>
-          <p className="font-semibold">
-            <span className="mr-2">
-              <FaAddressCard className="text-gray-600 inline" />
-            </span>
-            Plat: {data.car.plat}
-          </p>
-          <p className="font-semibold">
-            <span className="mr-2">
-              <IoMdCalendar className="text-gray-600 inline" />
-            </span>
-            Year: {data.car.year}
-          </p>
-          <p className="font-semibold">
-            <span className="mr-2">
-              <GiKeyCard className="text-gray-600 inline" />
-            </span>
-            Transmission: {data.car.transmission}
-          </p>
-          <p className="font-semibold">
-            <span className="mr-2">
-              <BsFillFuelPumpFill className="text-gray-600 inline" />
-            </span>
-            Fuel: {data.car.BbmType}
-          </p>
-          <p className="font-semibold">
-            <span className="mr-2">
-              <MdAirlineSeatReclineNormal className="text-gray-600 inline" />
-            </span>
-            Seat: {data.car.seat} Seats
-          </p>
-          <p className="font-semibold">
-            <span className="mr-2">
-              <IoMdSpeedometer className="text-gray-600 inline" />
-            </span>
-            Km: {data.car.kilometer}
-          </p>
-          <Link href={`/booking/${data.car._id}`}>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
-              Booking
-            </button>
-          </Link>
+
+        <div className="flex flex-row justify-center">
+          <div className="flex flex-row gap-5 border border-blue-400 rounded-lg p-8 text-black">
+            <p className="font-semibold">
+              <span className="mr-2">
+                <IoMdCar className="text-gray-600 inline" />
+              </span>
+              Tipe: {data.car.type}
+            </p>
+            <p className="font-semibold">
+              <span className="mr-2">
+                <FaAddressCard className="text-gray-600 inline" />
+              </span>
+              Plat: {data.car.plat}
+            </p>
+            <p className="font-semibold">
+              <span className="mr-2">
+                <IoMdCalendar className="text-gray-600 inline" />
+              </span>
+              Year: {data.car.year}
+            </p>
+            <p className="font-semibold">
+              <span className="mr-2">
+                <GiKeyCard className="text-gray-600 inline" />
+              </span>
+              Transmission: {data.car.transmission}
+            </p>
+            <p className="font-semibold">
+              <span className="mr-2">
+                <BsFillFuelPumpFill className="text-gray-600 inline" />
+              </span>
+              Fuel: {data.car.BbmType}
+            </p>
+            <p className="font-semibold">
+              <span className="mr-2">
+                <MdAirlineSeatReclineNormal className="text-gray-600 inline" />
+              </span>
+              Seat: {data.car.seat} Seats
+            </p>
+            <p className="font-semibold">
+              <span className="mr-2">
+                <IoMdSpeedometer className="text-gray-600 inline" />
+              </span>
+              Km: {data.car.kilometer}
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  </div>  
   );
 }
