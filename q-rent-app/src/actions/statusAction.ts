e1c0b3d4da312b4bfe0485c48931ee5560c41bd8
Arma Yoga Pratama <mgarmayogapratama@gmail.com>
2024-02-18 19:37:17 +0700

    Release 12 BookingById: done

---
+++ q-rent-app/src/actions/statusAction.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { BookingType } from "@/types/type";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+type MyResponse = {
+  message?: string;
+  data?: BookingType;
+};
+
+export async function ChangeStatus(
+  formData: FormData,
+  { params }: { params: { _id: string } }
+) {
+  const status = formData.get("status");
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/booking/status/${params._id}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        status: "unpaid",
+      }),
+      cache: "no-store",
+    }
+  );
+
+  const result = (await response.json()) as MyResponse;
+
+  if (!response.ok) {
+    return redirect(`/booking/status/${params._id}?error=${result.message}`);
+  }
+
+  revalidatePath(`/`, "layout");
+  return redirect(`/booking/status/${params._id}`);
+}
--- q-rent-app/src/app/booking/page.tsx
+++ q-rent-app/src/app/booking/page.tsx
@@ -19,36 +19,39 @@ async function dataBooking(): Promise<MyResponse> {
 }
 export default async function Booking() {
   const booking = await dataBooking();
+
   return (
     <div className="bg-white w-full h-screen mt-20">
-    <div className="bg-white w-full h-screen mt-28">
-    <h1 className="text-3xl font-bold text-center text-blue-400 mb-10">My Booking</h1>
-      <div className="overflow-x-auto text-black text-center">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Booking Id</th>
-              <th>Car Image</th>
-              <th>Car Name</th>
-              <th>Car Type</th>
-              <th>Car Brand</th>
-              <th>Color</th>
-              <th>Region</th>
-              <th>Booking Start</th>
-              <th>Booking End</th>
-              <th>Total Price</th>
-              <th>User</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {booking.listBooking.map((el, i) => (
-              <BookingAllCard booking={el} key={i} />
-            ))}
-          </tbody>
-        </table>
+      <div className="bg-white w-full h-screen mt-28">
+        <h1 className="text-3xl font-bold text-center text-blue-400 mb-10">
+          My Booking
+        </h1>
+        <div className="overflow-x-auto text-black text-center">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Booking Id</th>
+                <th>Car Image</th>
+                <th>Car Name</th>
+                <th>Car Type</th>
+                <th>Car Brand</th>
+                <th>Color</th>
+                <th>Region</th>
+                <th>Booking Start</th>
+                <th>Booking End</th>
+                <th>Total Price</th>
+                <th>User</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {booking.listBooking.map((el, i) => (
+                <BookingAllCard booking={el} key={i} />
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
