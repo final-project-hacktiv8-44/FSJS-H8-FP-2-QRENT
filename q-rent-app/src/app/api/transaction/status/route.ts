7aac26999b7e0708701431e4cc544d6253a02239
dewabrata15 <muhammaddewabrata@gmail.com>
2024-02-20 20:07:22 +0700

    midtrans & feedback

--- q-rent-app/src/app/api/transaction/status/route.ts
+++ q-rent-app/src/app/api/transaction/status/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(
   request: Request
-  // { params }: { params: { _id: string } }
+
 ) {
   try {
     const body = await request.json();
